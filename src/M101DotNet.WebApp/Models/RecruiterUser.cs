63601d16a228c4359de5f4c8ec422646c5b79f7a
akonopka <akonopka@pgs-soft.com>
2015-07-03 15:44:23 +0200

    Add RecruiterUser class + little fixes in main page css

---
+++ src/M101DotNet.WebApp/Models/RecruiterUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class RecruiterUser : User
+    {
+        public string CompanyName { get; set; }
+
+        public string CompanyDescription { get; set; }
+
+        public List<JobOffer> JobOffers { get; set; }
+    }
+
+}
--- src/M101DotNet.WebApp/Models/User.cs
+++ src/M101DotNet.WebApp/Models/User.cs
@@ -10,17 +10,11 @@ namespace WebApp.Models
 
         public string Name { get; set; }
 
+        public string Surname { get; set; }
+
         public string Email { get; set; }
 
         public string Password { get; set; }
-    }
-
-
-    public class RecruiterUser : User
-    {
-        public string PhoneNumber { get; set; }
-
-        public string CompanyProfile { get; set; }  
     }
 
     public class CandidateUser : User
