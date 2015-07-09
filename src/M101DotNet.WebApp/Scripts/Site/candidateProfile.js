2d22eb8ab5a7bb6311acdb0230f3d89c973f5641
akonopka <akonopka@pgs-soft.com>
2015-07-09 08:27:48 +0200

    candidateProfile.js - refactoring

--- src/M101DotNet.WebApp/Scripts/Site/candidateProfile.js
+++ src/M101DotNet.WebApp/Scripts/Site/candidateProfile.js
@@ -1,7 +1,7 @@
 ﻿
 $(document).ready(function () {
     $("#skills").on('click', ".delete-skill", function (event) {
-        $(this).parents(".skill-row").remove();
+        $(this).closest(".skill-row").remove();
     });
     $(".add-skill").click(function (event) {
         addRow("#skills");
@@ -12,18 +12,33 @@ var addRow = (function () {
     var index = $(".blank-row .row input[type='hidden']").val();
     return function (parent) {
         var blankRow = $(".blank-row").clone(false);
-        var newSkillNameId = "Skills[" + index + "].Name";
-        var newSkillLevelId = "Skills[" + index + "].Level";
-        var newSkillNameInput = blankRow.find(".skill-name input[type='text']");
-        var newSkillLevelInput = blankRow.find(".skill-level input[type='text']");
-        var newSkillIndex = blankRow.find("input[type='hidden']");
-        newSkillNameInput.attr("id", newSkillNameId).attr("name", newSkillNameId);
-        newSkillLevelInput.attr("id", newSkillLevelId).attr("name", newSkillLevelId);
-        newSkillIndex.attr("value", index);
-        blankRow.removeClass("blank-row hidden");
+        prepareSnippet(blankRow);
         blankRow.appendTo(parent);
         index++;
     };
 })();
 
+function addIdAndName (element, attrValue) {
+    element.attr("id", attrValue).attr("name", attrValue);
+}
 
+function prepareSnippet (blankRow, index) {
+    addIndexToSkill(blankRow, index);
+    addIdAndNameToSkill(blankRow, index);
+    blankRow.removeClass("blank-row hidden");
+    return blankRow;
+}
+
+function addIndexToSkill (blankRow, index){
+    var newSkillIndex = blankRow.find("input[type='hidden']");
+    newSkillIndex.attr("value", index);
+}
+
+function addIdAndNameToSkill (blankRow, index) {
+    var newSkillNameId = "Skills[" + index + "].Name";
+    var newSkillLevelId = "Skills[" + index + "].Level";
+    var newSkillNameInput = blankRow.find(".skill-name input[type='text']");
+    var newSkillLevelInput = blankRow.find(".skill-level input[type='text']");
+    addIdAndName(newSkillNameInput, newSkillNameId);
+    addIdAndName(newSkillLevelInput, newSkillLevelId);
+}
