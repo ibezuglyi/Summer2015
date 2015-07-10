6f5a95fb95484f0a975dd1b9791eb35d363865ec
akonopka <akonopka@pgs-soft.com>
2015-07-10 13:57:30 +0200

    Add DatabaseService and MappingService - without switching to new services.

---
+++ src/M101DotNet.WebApp/Services/IMappingService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApp.Entities;
+using WebApp.Models.Candidate;
+using WebApp.Models.Offer;
+using WebApp.Models.Recruiter;
+
+namespace WebApp.Services
+{
+    interface IMappingService
+    {
+        List<OfferViewModel> MapToOffersViewModel(List<JobOffer> offers);
+        OfferListViewModel MapToOfferViewModelList(List<OfferViewModel> offersModelView);
+        JobOffer MapToJobOffer(OfferModel model, string id);
+        OfferModel MapToOfferModel(JobOffer offer);
+        Skill MapToSkill(SkillModel model);
+        CandidateUser MapToCandidateUser(CandidateUserModel candidateModel);
+        CandidateUserModel MapToCandidateUserModel(CandidateUser candidate);
+        RecruiterModel MapToRecruiterModel(RecruiterUser recruiter);
+        List<Skill> MapSkillModelsToSkills(List<SkillModel> skillModels);
+        List<SkillModel> MapSkillsToSkillModels(List<Skill> skills);
+    }
+}
