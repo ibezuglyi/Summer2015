24a9e59bbebce40bd428ec8eaa2803f7e6078d50
Ievgen Bezuglyi <ievgen.bezuglyi@gmail.com>
2015-07-21 11:19:46 +0200

    added test project

---
+++ WebApp.Tests/Test1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace WebApp.Tests
+{
+    [TestFixture]
+    public class Test1
+    {
+        [TestCase]
+        public void CanRunTest()
+        {
+            Assert.AreEqual(2 + 2, 4);
+        }
+    }
+}
